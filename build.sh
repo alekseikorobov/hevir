f534b298c08a9dbf38daf8b8adfdb236e60e9399
aleksei korobov <aleksei_korobov@mail.ru>
2023-08-13 23:42:19 +0300

    change linux to single file

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
-rm dist -r
+rm dist_linux -r
 rm build -r
 rm hevir.spec
-pyinstaller hevir.py --hidden-import='PIL._tkinter_finder'
+pyinstaller hevir.py --hidden-import='PIL._tkinter_finder' --onefile
 
 mv dist dist_linux
